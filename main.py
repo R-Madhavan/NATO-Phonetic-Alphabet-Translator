dc12e95a3c46ac3dafcfe61e3768c2aed2f5ce36
Madhavan <127614873+R-Madhavan@users.noreply.github.com>
2024-11-01 15:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas
-flag = True
-while flag:
-    #Created a dictionary in this format {"A": "Alfa", "B": "Bravo, "C": "Charlie"}
-    data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+should_continue = True
+while should_continue:
     data_DF = pandas.DataFrame(data)
     phonetic_dict = {row.letter: row.code for (index, row) in data_DF.iterrows()}
 
-    #Created a list of the phonetic code words from a word that the user inputs.
     user_word = input("Enter the name?: ").upper()
-    nato_list = [phonetic_dict[letter] for letter in user_word]
-    print(nato_list)
+    try:
+        nato_list = [phonetic_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed")
+    else:
+        print(nato_list)
+        
